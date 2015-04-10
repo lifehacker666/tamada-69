f01870fde871d6818bd6d3e0f6e7c9fa06e75a75
lifehacker666 <lifehacker666@gmail.com>
2015-04-10 12:11:15 +0300

    htacees fix, error 404 page

---
+++ e404.php
@@ -0,0 +1,14 @@
+<?
+    $title = 'Страница не найдена (ошибка 404)';
+    $keywords = '';
+    $description = '';
+    $page_name = '';
+    include "templates/header.php";
+?>
+
+<!--center-block-->
+<div class="floatblock center-full">
+   <h1>Страница не найдена (ошибка 404)</h1>
+</div>
+
+<? include "templates/footer.php"; ?>
